0a62b28e89828f2e67865917651a9b135cedb94e
Qihang Yang <442621753@qq.com>
2019-04-22 00:54:35 +0800

    req

--- request.go
+++ request.go
@@ -2,129 +2,459 @@ package main
 
 import (
 	"bytes"
+	"compress/gzip"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
+	"net/http/cookiejar"
+	"net/http/httputil"
+	"net/url"
+	"os"
 	"strings"
-
-	"github.com/yqh231/Urezin/log"
+	"time"
 )
 
-type RequestCompose struct {
-	request *http.Request
+var VERSION string = "0.6"
+
+type Request struct {
+	httpreq *http.Request
+	Header  *http.Header
+	Client  *http.Client
+	Debug   int
+	Cookies []*http.Cookie
 }
 
-func NewReqCompose(method, url string, values interface{}) *RequestCompose {
+type Response struct {
+	R       *http.Response
+	content []byte
+	text    string
+	req     *Request
+}
 
-	if method != "GET" && method != "DELETE" {
-		req, _ := http.NewRequest(method, url, strings.NewReader(values.(string)))
-		return &RequestCompose{
-			req,
+type Header map[string]string
+type Params map[string]string
+type Datas map[string]string // for post form
+type Files map[string]string // name ,filename
+
+// {username,password}
+type Auth []string
+
+func Requests() *Request {
+
+	req := new(Request)
+
+	req.httpreq = &http.Request{
+		Method:     "GET",
+		Header:     make(http.Header),
+		Proto:      "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+	}
+	req.Header = &req.httpreq.Header
+	req.httpreq.Header.Set("User-Agent", "Go-Requests "+VERSION)
+
+	req.Client = &http.Client{}
+
+	// auto with Cookies
+	// cookiejar.New source code return jar, nil
+	jar, _ := cookiejar.New(nil)
+
+	req.Client.Jar = jar
+
+	return req
+}
+
+// Get ,req.Get
+
+func Get(origurl string, args ...interface{}) (resp *Response, err error) {
+	req := Requests()
+
+	// call request Get
+	resp, err = req.Get(origurl, args...)
+	return resp, err
+}
+
+func (req *Request) Get(origurl string, args ...interface{}) (resp *Response, err error) {
+	// set params ?a=b&b=c
+	//set Header
+	params := []map[string]string{}
+
+	//reset Cookies,
+	//Client.Do can copy cookie from client.Jar to req.Header
+	delete(req.httpreq.Header, "Cookie")
+
+	for _, arg := range args {
+		switch a := arg.(type) {
+		// arg is Header , set to request header
+		case Header:
+
+			for k, v := range a {
+				req.Header.Set(k, v)
+			}
+			// arg is "GET" params
+			// ?title=website&id=1860&from=login
+		case Params:
+			params = append(params, a)
+		case Auth:
+			// a{username,password}
+			req.httpreq.SetBasicAuth(a[0], a[1])
 		}
-
 	}
 
-	req, er := http.NewRequest(method, url, nil)
-	if er != nil {
-		log.Error.Println(er.Error())
+	disturl, _ := buildURLParams(origurl, params...)
+
+	//prepare to Do
+	URL, err := url.Parse(disturl)
+	if err != nil {
+		return nil, err
+	}
+	req.httpreq.URL = URL
+
+	req.ClientSetCookies()
+
+	req.RequestDebug()
+
+	res, err := req.Client.Do(req.httpreq)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	resp = &Response{}
+	resp.R = res
+	resp.req = req
+	resp.ResponseDebug()
+	return resp, nil
+}
+
+// handle URL params
+func buildURLParams(userURL string, params ...map[string]string) (string, error) {
+	parsedURL, err := url.Parse(userURL)
+
+	if err != nil {
+		return "", err
+	}
+
+	parsedQuery, err := url.ParseQuery(parsedURL.RawQuery)
+
+	if err != nil {
+		return "", nil
+	}
+
+	for _, param := range params {
+		for key, value := range param {
+			parsedQuery.Add(key, value)
+		}
+	}
+	return addQueryParams(parsedURL, parsedQuery), nil
+}
+
+func addQueryParams(parsedURL *url.URL, parsedQuery url.Values) string {
+	if len(parsedQuery) > 0 {
+		return strings.Join([]string{strings.Replace(parsedURL.String(), "?"+parsedURL.RawQuery, "", -1), parsedQuery.Encode()}, "?")
+	}
+	return strings.Replace(parsedURL.String(), "?"+parsedURL.RawQuery, "", -1)
+}
+
+func (req *Request) RequestDebug() {
+
+	if req.Debug != 1 {
+		return
+	}
+
+	fmt.Println("===========Go RequestDebug ============")
+
+	message, err := httputil.DumpRequestOut(req.httpreq, false)
+	if err != nil {
+		return
+	}
+	fmt.Println(string(message))
+
+	if len(req.Client.Jar.Cookies(req.httpreq.URL)) > 0 {
+		fmt.Println("Cookies:")
+		for _, cookie := range req.Client.Jar.Cookies(req.httpreq.URL) {
+			fmt.Println(cookie)
+		}
+	}
+}
+
+// cookies
+// cookies only save to Client.Jar
+// req.Cookies is temporary
+func (req *Request) SetCookie(cookie *http.Cookie) {
+	req.Cookies = append(req.Cookies, cookie)
+}
+
+func (req *Request) ClearCookies() {
+	req.Cookies = req.Cookies[0:0]
+}
+
+func (req *Request) ClientSetCookies() {
+
+	if len(req.Cookies) > 0 {
+		// 1. Cookies have content, Copy Cookies to Client.jar
+		// 2. Clear  Cookies
+		req.Client.Jar.SetCookies(req.httpreq.URL, req.Cookies)
+		req.ClearCookies()
+	}
+
+}
+
+// set timeout s = second
+func (req *Request) SetTimeout(n time.Duration) {
+	req.Client.Timeout = time.Duration(n * time.Second)
+}
+
+func (req *Request) Proxy(proxyurl string) {
+
+	urli := url.URL{}
+	urlproxy, err := urli.Parse(proxyurl)
+	if err != nil {
+		fmt.Println("Set proxy failed")
+		return
+	}
+	req.Client.Transport = &http.Transport{
+		Proxy:           http.ProxyURL(urlproxy),
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+}
+
+/**************/
+func (resp *Response) ResponseDebug() {
+
+	if resp.req.Debug != 1 {
+		return
+	}
+
+	fmt.Println("===========Go ResponseDebug ============")
+
+	message, err := httputil.DumpResponse(resp.R, false)
+	if err != nil {
+		return
+	}
+
+	fmt.Println(string(message))
+
+}
+
+func (resp *Response) Content() []byte {
+
+	defer resp.R.Body.Close()
+	var err error
+
+	var Body = resp.R.Body
+	if resp.R.Header.Get("Content-Encoding") == "gzip" && resp.req.Header.Get("Accept-Encoding") != "" {
+		// fmt.Println("gzip")
+		reader, err := gzip.NewReader(Body)
+		if err != nil {
+			return nil
+		}
+		Body = reader
+	}
+
+	resp.content, err = ioutil.ReadAll(Body)
+	if err != nil {
 		return nil
 	}
-	if values == nil {
-		return &RequestCompose{
-			req,
+
+	return resp.content
+}
+
+func (resp *Response) Text() string {
+	if resp.content == nil {
+		resp.Content()
+	}
+	resp.text = string(resp.content)
+	return resp.text
+}
+
+func (resp *Response) SaveFile(filename string) error {
+	if resp.content == nil {
+		resp.Content()
+	}
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write(resp.content)
+	f.Sync()
+
+	return err
+}
+
+func (resp *Response) Json(v interface{}) error {
+	if resp.content == nil {
+		resp.Content()
+	}
+	return json.Unmarshal(resp.content, v)
+}
+
+func (resp *Response) Cookies() (cookies []*http.Cookie) {
+	httpreq := resp.req.httpreq
+	client := resp.req.Client
+
+	cookies = client.Jar.Cookies(httpreq.URL)
+
+	return cookies
+
+}
+
+/**************post*************************/
+// call req.Post ,only for easy
+func Post(origurl string, args ...interface{}) (resp *Response, err error) {
+	req := Requests()
+
+	// call request Get
+	resp, err = req.Post(origurl, args...)
+	return resp, err
+}
+
+// POST requests
+
+func (req *Request) Post(origurl string, args ...interface{}) (resp *Response, err error) {
+
+	req.httpreq.Method = "POST"
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	// set params ?a=b&b=c
+	//set Header
+	params := []map[string]string{}
+	datas := []map[string]string{} // POST
+	files := []map[string]string{} //post file
+
+	//reset Cookies,
+	//Client.Do can copy cookie from client.Jar to req.Header
+	delete(req.httpreq.Header, "Cookie")
+
+	for _, arg := range args {
+		switch a := arg.(type) {
+		// arg is Header , set to request header
+		case Header:
+
+			for k, v := range a {
+				req.Header.Set(k, v)
+			}
+			// arg is "GET" params
+			// ?title=website&id=1860&from=login
+		case Params:
+			params = append(params, a)
+
+		case Datas: //Post form data,packaged in body.
+			datas = append(datas, a)
+		case Files:
+			files = append(files, a)
+		case Auth:
+			// a{username,password}
+			req.httpreq.SetBasicAuth(a[0], a[1])
 		}
 	}
-	q := req.URL.Query()
-	for k, v := range values.(map[string]string) {
-		q.Add(k, v)
-	}
-	req.URL.RawQuery = q.Encode()
-	return &RequestCompose{
-		req,
-	}
-}
 
-func (r *RequestCompose) SetHeader(headers map[string]string) {
-	for k, v := range headers {
-		r.request.Header.Add(k, v)
-	}
-	r.request.Header.Add("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Mobile Safari/537.36")
-	r.request.Header.Add("Refer", "http://untwallet.com/account/sign_in")
-	r.request.Header.Add("Host", "untwallet.com")
-	r.request.Header.Add("Accept", "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01")
-}
+	disturl, _ := buildURLParams(origurl, params...)
 
-func (r *RequestCompose) ResHandle(res *http.Response, data interface{}) {
-	body, err := ioutil.ReadAll(res.Body)
+	if len(files) > 0 {
+		req.buildFilesAndForms(files, datas)
+
+	} else {
+		Forms := req.buildForms(datas...)
+		req.setBodyBytes(Forms) // set forms to body
+	}
+	//prepare to Do
+	URL, err := url.Parse(disturl)
 	if err != nil {
-		log.Error.Println(err.Error())
-		return
+		return nil, err
 	}
-	err = json.Unmarshal(body, data)
+	req.httpreq.URL = URL
+
+	req.ClientSetCookies()
+
+	req.RequestDebug()
+
+	res, err := req.Client.Do(req.httpreq)
+
 	if err != nil {
-		switch t := data.(type) {
-		case *string:
-			*t = string(body)
-		case *[]byte:
-			*t = []byte(body)
-		}
+		fmt.Println(err)
+		return nil, err
 	}
+
+	resp = &Response{}
+	resp.R = res
+	resp.req = req
+	resp.ResponseDebug()
+	return resp, nil
 }
 
-func (r *RequestCompose) GetResponseDirect(res *http.Response) string {
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Error.Println(err.Error())
-		return ""
-	}
-	return string(body)
+// only set forms
+func (req *Request) setBodyBytes(Forms url.Values) {
+
+	// maybe
+	data := Forms.Encode()
+	req.httpreq.Body = ioutil.NopCloser(strings.NewReader(data))
+	req.httpreq.ContentLength = int64(len(data))
 }
 
-func (r *RequestCompose) GetReq() *http.Request {
-	return r.request
-}
+// upload file and form
+// build to body format
+func (req *Request) buildFilesAndForms(files []map[string]string, datas []map[string]string) {
 
-func Upload(client *http.Client, url string, values map[string]io.Reader) (res *http.Response, err error) {
+	//handle file multipart
+
 	var b bytes.Buffer
 	w := multipart.NewWriter(&b)
-	for key, r := range values {
-		var fw io.Writer
-		if x, ok := r.(io.Closer); ok {
-			defer x.Close()
-		}
-		if key == "image" {
-			if fw, err = w.CreateFormFile(key, "image"); err != nil {
-				return
-			}
-		} else {
-			if fw, err = w.CreateFormField(key); err != nil {
-				return
-			}
-		}
-		// Add other fields
-		if _, err = io.Copy(fw, r); err != nil {
-			return nil, err
-		}
 
+	for _, file := range files {
+		for k, v := range file {
+			part, err := w.CreateFormFile(k, v)
+			if err != nil {
+				fmt.Printf("Upload %s failed!", v)
+				panic(err)
+			}
+			file := openFile(v)
+			_, err = io.Copy(part, file)
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
+
+	for _, data := range datas {
+		for k, v := range data {
+			w.WriteField(k, v)
+		}
+	}
+
 	w.Close()
-
-	req, err := http.NewRequest("POST", url, &b)
-	if err != nil {
-		return
-	}
+	// set file header example:
+	// "Content-Type": "multipart/form-data; boundary=------------------------7d87eceb5520850c",
+	req.httpreq.Body = ioutil.NopCloser(bytes.NewReader(b.Bytes()))
+	req.httpreq.ContentLength = int64(b.Len())
 	req.Header.Set("Content-Type", w.FormDataContentType())
+}
 
-	res, err = client.Do(req)
+// build post Form data
+func (req *Request) buildForms(datas ...map[string]string) (Forms url.Values) {
+	Forms = url.Values{}
+	for _, data := range datas {
+		for key, value := range data {
+			Forms.Add(key, value)
+		}
+	}
+	return Forms
+}
+
+// open file for post upload files
+
+func openFile(filename string) *os.File {
+	r, err := os.Open(filename)
 	if err != nil {
-		return
+		panic(err)
 	}
-
-	if res.StatusCode != http.StatusOK {
-		err = fmt.Errorf("bad status: %s", res.Status)
-	}
-	return
+	return r
 }
